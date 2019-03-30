6ed8bcffcd761ef1d1bc5338da817caf5d11bcef
lhan <540457096@qq.com>
2019-03-30 15:15:58 +0800

    [#lhan]update thread interrupt demo

--- src/main/java/com/github/linhan111/T201903/ThreadInterrupt.java
+++ src/main/java/com/github/linhan111/T201903/ThreadInterrupt.java
@@ -23,6 +23,9 @@ public class ThreadInterrupt {
         System.out.println("设置子线程的中断状态为已中断");
         t1.interrupt();
         System.out.println(Thread.interrupted());
+
+        // 注意这个static方法与public void interrupt()这两个方法的区别，可以参考他们的注释文档
+        Thread.interrupted();
     }
     // 每个线程都有一个中断标志位，但是只有线程为waiting或者time_waiting状态时设置线程中断状态为true时才会抛出InterruptedException
 }
