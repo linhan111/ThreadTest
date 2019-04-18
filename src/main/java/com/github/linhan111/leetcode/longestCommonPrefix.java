9d0ca4519d9fbcf945319f31cec4e1982ca6a021
linhan111 <540457096@qq.com>
2019-04-18 13:56:24 +0800

    add leetcode上求字符串数组最长公共前缀题目

--- src/main/java/com/github/linhan111/leetcode/longestCommonPrefix.java
+++ src/main/java/com/github/linhan111/leetcode/longestCommonPrefix.java
@@ -2,40 +2,50 @@ package com.github.linhan111.leetcode;
 
 /**
  * 问题描述：https://leetcode-cn.com/explore/learn/card/array-and-string/200/introduction-to-string/781/
+ * 问题备注：求的是字符串数组的最长公共前缀？前缀和中间截取的字符串的区别搞清楚！！！
  */
 public class longestCommonPrefix {
     public static void main(String[] args) {
-        String[] x = {"asdf", "sd", "asd"};
+        String[] x = {"asdf","sd","asd"}; // 这个数组应该不存在最长公共前缀？
         System.out.println("result + " + longestCommonPrefix(x));
     }
 
     // 求字符串数组的最长公共前缀
     private static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        if (strs.length == 1) return strs[0];
         // 最长元素的index与length
-        int minSizeElementIndex = 0;
-        int minSizeElementLength = strs[0].length();
+        int maxSizeElementIndex = 0;
+        int maxSizeElementLength = strs[0].length();
         // 由于上面直接赋值了第0个元素的index与length，所有这个loop可直接从第1个元素开始循环
         for (int i = 1; i < strs.length; i++) {
             if ("".equals(strs[i])) {
                 return "";
             }
-            if (strs[i].length() < minSizeElementLength) {
-                minSizeElementLength = strs[i].length();
-                minSizeElementIndex = i;
+            if (strs[i].length() > maxSizeElementLength) {
+                maxSizeElementLength = strs[i].length();
+                maxSizeElementIndex = i;
             }
         }
 
-        for (int length = strs[minSizeElementIndex].length(); length > 0; length--) {
-            for (String s : strs) {
-                if (s.contains(strs[minSizeElementIndex].substring(0, length))) {
-                    return strs[minSizeElementIndex].substring(length);
+        for (int length = strs[maxSizeElementIndex].length(); length > 0; length--) {
+            boolean flag = true;
+            for (int i = 0; i < strs.length; i++) {
+                // 取消loop对数组中length最长的元素
+                if (i != maxSizeElementIndex) {
+                    if (!strs[i].startsWith(strs[maxSizeElementIndex].substring(0, length))) {
+                        flag = false;
+                    }
                 }
+            }
+            if (flag) {
+                return strs[maxSizeElementIndex].substring(0, length);
             }
         }
         return "";
     }
 
-    // 这种解法可能存在问题，参考链接：https://blog.csdn.net/fengpojian/article/details/81326781
+    // 这种解法可能存在问题？参考链接：https://blog.csdn.net/fengpojian/article/details/81326781
     private static String theOtherTest(String[] strs) {
         int count = strs.length;
         String prefix = "";
