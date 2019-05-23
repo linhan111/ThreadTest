24bb94eba004647f86eefadb247f8a9a0e2e6b92
linhan111 <540457096@qq.com>
2019-05-23 09:15:21 +0800

    添加docker/netty book

---
+++ src/main/java/com/github/linhan111/book/book.java
@@ -0,0 +1,13 @@
+package com.github.linhan111.book;
+
+/**
+ * 添加一下其他的书，大都是gitbook链接
+ * 常量定义在interface与class中的区别：https://www.cnblogs.com/EasonJim/p/7871753.html
+ */
+public interface book {
+    // docker-从入门到实践
+    String docker_gitbook = "https://yeasy.gitbooks.io/docker_practice/content/";
+
+    // netty实战
+    String netty_gitbook = "https://waylau.gitbooks.io/essential-netty-in-action/content/";
+}
