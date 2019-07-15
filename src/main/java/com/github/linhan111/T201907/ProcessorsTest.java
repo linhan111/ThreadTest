c096223cea121d74859e7279975157952f3b0a61
linhan111 <540457096@qq.com>
2019-07-15 16:13:37 +0800

    增加判断BigDecimal是否为整数的方法，增加availableProcessors的测试（输出线程数而非核心数）

---
+++ src/main/java/com/github/linhan111/T201907/ProcessorsTest.java
@@ -0,0 +1,14 @@
+package com.github.linhan111.T201907;
+
+public class ProcessorsTest {
+    public static void main(String[] args) {
+        // 4C8T的cpu，availableProcessors为线程数而非核心数
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        System.out.println(Runtime.getRuntime().freeMemory());
+        System.out.println(Runtime.getRuntime().totalMemory());
+//         jvm 启动参数为：
+//        -Xms20m
+//        -Xmx20m
+//        -Xmn10M
+    }
+}
--- src/main/java/com/github/linhan111/T201907/ThreadLocalTest.java
+++ src/main/java/com/github/linhan111/T201907/ThreadLocalTest.java
@@ -1,9 +1,11 @@
 package com.github.linhan111.T201907;
 
+import java.math.BigDecimal;
+
 public class ThreadLocalTest {
     private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         threadLocal.set(Thread.currentThread().getName());
         // key为当前threa，value为set的值，会覆盖，注意ThreadLocalMap中的Entry保持的是和当前线程的WeakReference，方便gc
         threadLocal.set("this is test");
@@ -14,9 +16,20 @@ public class ThreadLocalTest {
             System.out.println(threadLocal.get());
 //            threadLocal.remove();
         }).start();
-    }
+    }*/
 
     // 注意ThreadLocal的内存溢出问题，WeakReference中的key-value，value保持了一个强引用，
     // 在线程回收之前不会回收这一块堆内存，通过声明ThreadLocal为static或手动调用remove解决
     // 参考：https://www.jianshu.com/p/250798f9ff76
+
+    public static void main(String[] args) {
+        BigDecimal b = new BigDecimal("1.1");
+        BigDecimal x = new BigDecimal(b.intValue());
+        System.out.println(x.equals(b));
+
+        // 该方法可判断BigDecimal是否为整数！！！暂时只想到这一种方法
+        BigDecimal b1 = new BigDecimal("2");
+        BigDecimal x1 = new BigDecimal(b1.intValue());
+        System.out.println(x1.equals(b1));
+    }
 }
