7b86d2aaa2c76f88db710c2048a20347fd353430
linhan111 <540457096@qq.com>
2020-06-29 13:55:04 +0800

    restudy bubble sort

--- src/main/java/com/github/linhan111/T201902/pai_xu_suan_fa.java
+++ src/main/java/com/github/linhan111/T201902/pai_xu_suan_fa.java
@@ -1,5 +1,7 @@
 package com.github.linhan111.T201902;
 
+import java.util.Arrays;
+
 /**
  * @author lhan
  * @version 1.0.0
@@ -24,7 +26,9 @@ public class pai_xu_suan_fa {
         }
         for (int i = 0; i < array.length; i++) {
             // 这里在我的记忆中是array.length -1次循环，注意这里的区别，可提高性能
-            for (int j = 0; j < array.length - 1 - i; j++) {
+            // 个人理解：把最大的数往后排，一次循环之后最后的数是最大的，下次循环排除掉最后的那个数，这是array.length - i的含义
+            // 当然这里j < array.length - 1 也行，就是循环次数会多一些
+            for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j + 1] < array[j]) {
                     int temp = array[j + 1];
                     array[j + 1] = array[j];
@@ -47,7 +51,7 @@ public class pai_xu_suan_fa {
         }
         for (int i = 0; i < array.length; i++) {
             int minIndex = i;
-            for(int j = i; j<array.length; j++) {
+            for (int j = i; j < array.length; j++) {
                 if (array[j] < array[minIndex]) {
                     minIndex = j;
                 }
@@ -61,8 +65,7 @@ public class pai_xu_suan_fa {
 
     public static void main(String[] args) {
         int[] test = new int[]{1, 1000, 45, 6, 234, 678, 100};
-        // selection_sort(test).sout可在idea中直接打印出执行结果
-        // System.out.println(bubble_sort(test));
-        System.out.println(selection_sort(test));
+         System.out.println(Arrays.toString(bubble_sort(test)));
+//        System.out.println(Arrays.toString(selection_sort(test)));
     }
 }
