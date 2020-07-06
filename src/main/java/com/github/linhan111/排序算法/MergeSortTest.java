bb0b49bcd64048dacc7362e53283704cbcc077c5
linhan111 <540457096@qq.com>
2020-07-06 11:19:13 +0800

    restudy merge sort

--- src/main/java/com/github/linhan111/排序算法/MergeSortTest.java
+++ src/main/java/com/github/linhan111/排序算法/MergeSortTest.java
@@ -1,17 +1,18 @@
-package com.github.linhan111.T201909;
+package com.github.linhan111.排序算法;
+
+import java.util.Arrays;
 
 /**
- * 归并排序Demo，注意归并排序的使用场景
- * 在ShardingSphere中，需要对order by等关键字查询的分表结果聚合，实现方式：得到的两个有序集合，通过归并排序组合成最终的结果
+ * 归并排序Demo
+ * 注意归并排序的使用场景：在ShardingSphere中，需要对order by等关键字查询的分表结果聚合，实现方式：得到的两个有序集合，通过归并排序组合成最终的结果
  * 参考链接：
  * https://blog.csdn.net/qq_39776901/article/details/77151983
  * https://www.jianshu.com/p/95c69f8e47e7
  *
  * @author linhan111
  */
-public class SortMergeTest {
+public class MergeSortTest {
     public static void main(String[] args) {
-        // 定义一个无序数组
         int[] arr = {3, 1, 2, 4, 5, 7, 6, 8};
         // left在这里定义，控制左半部分的首元素
         int left = 0;
@@ -19,30 +20,26 @@ public class SortMergeTest {
         int right = arr.length - 1;
         // 调用排序函数
         sort(arr, left, right);
-        // 输出排序后的数组
-        for (int value : arr) {
-            System.out.println(value);
-        }
+
+        System.out.println(Arrays.toString(arr));
     }
 
     private static int[] sort(int[] arr, int left, int right) {
-        // mid其实是作为分割的指针
+        // mid是作为分割的指针
         int mid = (left + right) / 2;
-        // 递归拆分，调用归并（这里是关键，需要读者好好思考）
-        // 读者可以用元素少一点的数组先思考明白了，再去想递归是什么回事
+        // 递归拆分，调用归并（这里是关键，需要好好思考）
         if (left < right) {
             sort(arr, left, mid);
             sort(arr, mid + 1, right);
-            merger(arr, left, right, mid);
+            merge(arr, left, right, mid);
         }
         return arr;
     }
 
-    // 归并函数
-    private static void merger(int[] arr, int left, int right, int mid) {
+    private static void merge(int[] arr, int left, int right, int mid) {
         // 定义一个新数组来存放排序后的数
         int[] temp = new int[right - left + 1];
-        // x控制新数组的下表
+        // x控制新数组的下标
         int x = 0;
         // j指向右半部分数组的首元素
         int j = mid + 1;
--- src/main/java/com/github/linhan111/排序算法/pai_xu_suan_fa.java
+++ src/main/java/com/github/linhan111/排序算法/pai_xu_suan_fa.java
@@ -1,4 +1,4 @@
-package com.github.linhan111.T201902;
+package com.github.linhan111.排序算法;
 
 import java.util.Arrays;
 
@@ -28,12 +28,18 @@ public class pai_xu_suan_fa {
             // 这里在我的记忆中是array.length -1次循环，注意这里的区别，可提高性能
             // 个人理解：把最大的数往后排，一次循环之后最后的数是最大的，下次循环排除掉最后的那个数，这是array.length - i的含义
             // 当然这里j < array.length - 1 也行，就是循环次数会多一些
+            // 增加一个标志位flag，如果某次循环没有交换顺序则数组已有序，直接跳出外层循环
+            boolean flag = true;
             for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j + 1] < array[j]) {
                     int temp = array[j + 1];
                     array[j + 1] = array[j];
                     array[j] = temp;
+                    flag = false;
                 }
+            }
+            if (flag) {
+                break;
             }
         }
         return array;
@@ -51,6 +57,8 @@ public class pai_xu_suan_fa {
         }
         for (int i = 0; i < array.length; i++) {
             int minIndex = i;
+            // 每次遍历，[0, j)是有序区，[j, array.length]是无序区
+            // 先找到最小的元素下标，将它与j互换位置，j是有序区与无序区的分割线
             for (int j = i; j < array.length; j++) {
                 if (array[j] < array[minIndex]) {
                     minIndex = j;
@@ -63,9 +71,33 @@ public class pai_xu_suan_fa {
         return array;
     }
 
+    /**
+     * 插入排序
+     *
+     * @param array 原数组
+     * @return 有序数组
+     */
+    private static int[] insertion_sort(int[] array) {
+        if (array.length == 0) {
+            return array;
+        }
+        for (int i = 0; i < array.length - 1; i++) {
+            int current = array[i + 1];
+            int preIndex = i;
+            while (preIndex >= 0 && current < array[preIndex]) {
+                array[preIndex + 1] = array[preIndex];
+                preIndex--;
+            }
+            array[preIndex + 1] = current;
+        }
+        return array;
+    }
+
     public static void main(String[] args) {
-        int[] test = new int[]{1, 1000, 45, 6, 234, 678, 100};
-         System.out.println(Arrays.toString(bubble_sort(test)));
+//        int[] test = new int[]{1, 1000, 45, 6, 234, 678, 100};
+        int[] test = new int[]{8, 0};
+//         System.out.println(Arrays.toString(bubble_sort(test)));
 //        System.out.println(Arrays.toString(selection_sort(test)));
+        System.out.println(Arrays.toString(insertion_sort(test)));
     }
 }
